bee83fdeffcf718cf4c0ff9a19ca73944b820282
bhuvan malik <bhuvanmalik007@gmail.com>
2017-05-02 18:07:16 +0530

    oxford api integration

--- routes/globalsearch.js
+++ routes/globalsearch.js
@@ -4,11 +4,35 @@ const ResponseBuilder = require('./responsebuilder');
 const request = require('request-promise');
 
 
+// router.get('/:word', (req, res, next) => {
+//   request({ uri: 'http://owlbot.info/api/v1/dictionary/'+req.params.word, method: 'GET' })
+//     .then(function(response) {
+//       console.log(response);
+//       res.send(JSON.parse(response));
+//     });
+// })
+
 router.get('/:word', (req, res, next) => {
-  request({ uri: 'http://owlbot.info/api/v1/dictionary/'+req.params.word, method: 'GET' })
+  request({ uri: 'https://od-api.oxforddictionaries.com:443/api/v1/entries/en/'+req.params.word,
+   method: 'GET',
+   headers: {
+       'Accept': 'application/json',
+       'app_id': 'd3900461',
+       'app_key': '16639cb92817985e70a5dfacb28a4cab'
+   }})
     .then(function(response) {
-      console.log(response);
-      res.send(JSON.parse(response));
+      let i = JSON.parse(response).results[0].lexicalEntries[0].entries[0].senses;
+      console.log(i.examples);
+      if(i[0].subsenses){
+        let j = {word: req.params.word,meaning: i[0].definitions[0], example: i[0].examples && i[0].examples[0].text};
+        let k = i[0].subsenses.map(obj=>({word: req.params.word, meaning: obj.definitions[0], example: obj.examples[0].text}));
+        res.send([j, ...k]);
+      }
+      else{
+        let j = i.map(obj=>({word: req.params.word, meaning: obj.definitions[0], example: obj.examples && obj.examples[0].text}));
+        res.send(j);
+      }
+      // res.send(JSON.parse(response));
     });
 })
 
