c007eb525d8f68da9215fe26a63c4470c6d2c6bb
bhuvan malik <bhuvanmalik007@gmail.com>
2017-05-14 18:43:14 +0530

    improved logicdddddoogic, added route for bulk upload

--- routes/globalsearch.js
+++ routes/globalsearch.js
@@ -1,10 +1,45 @@
 var express = require('express');
 var router = express.Router();
+const mongojs = require('mongojs');
+const db = mongojs('mongodb://root:admin@ds039684.mlab.com:39684/apptest', ['words']);
 const ResponseBuilder = require('./responsebuilder');
 const request = require('request-promise');
+const createWordsArray = require('./oxfordJsonParser');
+
+
+router.post('/multiple', (req, res, next) => {
+  const promiseArray = req.body.words.map((word) =>
+    request({
+      uri: 'https://od-api.oxforddictionaries.com:443/api/v1/entries/en/' + word,
+      method: 'GET',
+      resolveWithFullResponse: true,
+      headers: {
+        'Accept': 'application/json',
+        'app_id': 'd3900461',
+        'app_key': '16639cb92817985e70a5dfacb28a4cab'
+      }
+    })
+  );
+  Promise.all(promiseArray.map(p => p.catch(() => undefined)))
+    .then(responses => {
+      let wordsArray = responses.reduce((acc, response) => {
+        const body = JSON.parse(response.body);
+        const singleWordArray = createWordsArray(body.results[0].lexicalEntries[0].entries[0].senses, body.results[0].word);
+        return [...acc, ...singleWordArray];
+      }, []);
+        db.words.insert(wordsArray, (err, wordArray) => {
+          err && res.json(ResponseBuilder(err, false));
+          res.json(ResponseBuilder({ wordsArray, message: 'records saved' }, true));
+        });
+    }, reason => {
+      console.log('reason')
+      res.send(reason);
+    });
+})
+
 
 router.get('/', (req, res, next) => {
-  res.send({words:[]});
+  res.send({ words: [] });
 })
 
 
@@ -20,24 +55,12 @@ router.get('/:word', (req, res, next) => {
       }
     })
     .then(response => {
-      console.log(response.statusCode);
-      if (response.statusCode != 200) {
-        res.send([]);
-        return;
-      }
-      let i = JSON.parse(response.body).results[0].lexicalEntries[0].entries[0].senses;
-      if (i[0].subsenses) {
-        let j = { word: req.params.word, meaning: i[0].definitions[0], example: i[0].examples && i[0].examples[0].text };
-        let k = i[0].subsenses.map(obj => ({ word: req.params.word, meaning: obj.definitions[0], example: obj.examples && obj.examples[0].text }));
-        res.send({words: [j, ...k], pronounciation: JSON.parse(response.body).results[0].lexicalEntries[0].pronunciations[0].audioFile});
-      } else {
-        let j = i.map(obj => ({ word: req.params.word, meaning: obj.definitions[0], example: obj.examples && obj.examples[0].text }));
-        res.send({words: j, pronounciation: JSON.parse(response.body).results[0].lexicalEntries[0].pronunciations[0].audioFile});
-      }
-    }, err => {
-      console.log(err);
-      res.send({words:[]});
-    });
+      const body = JSON.parse(response.body);
+      const wordsArray = createWordsArray(body.results[0].lexicalEntries[0].entries[0].senses, body.results[0].word);
+      const pronounciation = body.results[0].lexicalEntries[0].pronunciations[0].audioFile;
+      res.send({ pronounciation, words: wordsArray });
+    })
+    .catch((err) => res.send({ words: [] }));
 })
 
 module.exports = router;
